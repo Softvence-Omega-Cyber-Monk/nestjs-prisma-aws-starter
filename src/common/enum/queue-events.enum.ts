c898fe0873b09dd9b5b9d4d3ee781f75cfd1b3fc
shahadathhs <shahadathhossensajib732@gmail.com>
2025-11-27 23:12:35 +0600

    feat: common utils added

---
+++ src/common/enum/queue-events.enum.ts
@@ -0,0 +1,11 @@
+export enum EventsEnum {
+  // === Generic status events ===
+  ERROR = 'error', // Server -> Client: operation failed
+  SUCCESS = 'success', // Server -> Client: operation succeeded
+}
+
+export enum QueueEventsEnum {
+  // === Notification events ===
+  NOTIFICATION = 'queue:notification',
+  MESSAGES = 'queue:messages',
+}
